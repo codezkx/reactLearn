39e51c953a44d8114142b8d1b0bd7fee66506936
郑克祥 <72841383+codezkx@users.noreply.github.com>
2023-06-04 19:58:18 +0800

    chore： 删除不必要文件

--- React/day29_useId And useImperativeHandle/src/AddComment.js
+++
@@ -1,9 +0,0 @@
-import { forwardRef } from 'react';
-
-const AddCommentFn = function (props, ref) {
-  return <input placeholder="Add comment..." ref={ref} />;
-}
-
-const AddComment = forwardRef(AddCommentFn)
-
-export default  AddComment
